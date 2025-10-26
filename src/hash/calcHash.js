87894099ecd5ab6f2f61d07f7efb6609797a0eae
AnatolyFiv <64248582+AnatolyFiv@users.noreply.github.com>
2025-10-26 18:59:04 +0300

    feat(hash): implement SHA256 hash calculation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { dirname, join } from "path";
+import { pipeline } from "stream/promises";
+import { fileURLToPath } from "url";
+
 const calculateHash = async () => {
-  // Write your code here
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+
+  try {
+    await pipeline(createReadStream(filePath), hash);
+    console.log(hash.digest('hex'));
+  } catch {
+    throw new Error('Hash operation failed');
+  }
 };
 
 await calculateHash();
