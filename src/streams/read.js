d5ca0ea0ddc3dc6677b9c44c9f24cfad809a8754
AnatolyFiv <64248582+AnatolyFiv@users.noreply.github.com>
2025-10-26 21:20:07 +0300

    feat(streams): implement read, write, and transform stream functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from "fs";
+import { dirname, join } from "path";
+import { pipeline } from "stream/promises";
+import { fileURLToPath } from "url";
+
 const read = async () => {
-  // Write your code here
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    await pipeline(createReadStream(filePath), process.stdout, { end: false })
+    if (process.stdout.isTTY) {
+      process.stdout.write('\n');
+    }
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { pipeline } from "stream/promises";
+import { fileURLToPath } from "url";
+
 const write = async () => {
-  // Write your code here
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+
+  try {
+    await pipeline(process.stdin, createWriteStream(filePath));
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await write();
