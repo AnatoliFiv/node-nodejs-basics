4e9b1e69d52d94304e4ab0fac328cd182689d6e5
AnatolyFiv <64248582+AnatolyFiv@users.noreply.github.com>
2025-10-27 02:03:18 +0300

    feat(child-process): implement IPC between master and child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,27 @@
+import { spawn } from 'node:child_process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const childPath = join(__dirname, 'files', 'script.js');
+
+  const childProcess = spawn('node', [childPath, ...args], {
+    stdio: ['pipe', 'pipe', 'inherit']
+  });
+
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.pipe(process.stdout);
+
+  childProcess.on('error', (error) => {
+    console.error('Child process error:', error);
+  });
+
+  childProcess.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
+
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['testArg', 'testArg1', 'testArg2']);
