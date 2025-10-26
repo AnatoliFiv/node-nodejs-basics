ec06bb9e1a1fb368529d26c5b3388cb001eae13a
AnatolyFiv <64248582+AnatolyFiv@users.noreply.github.com>
2025-10-26 22:07:53 +0300

    feat(zip): implement compress and decompress functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { access } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-  // Write your code here
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const inputPath = join(__dirname, 'files', 'fileToCompress.txt');
+  const outputPath = join(__dirname, 'files', 'archive.gz');
+
+  try {
+    await access(inputPath);
+    await pipeline(createReadStream(inputPath), createGzip(), createWriteStream(outputPath));
+  } catch {
+    throw new Error("Operation failed");
+  }
 };
 
 await compress();
